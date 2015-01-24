b34edb64f0ae30046729c9051c42b07b54969482
Piers The Conqueror <solar_1992@hotmail.co.uk>
2015-01-24 10:51:21 +0000

    Fixed bool thingy

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -7,14 +7,20 @@ namespace UnitySampleAssets._2D
     {
         private bool facingRight = true; // For determining which way the player is currently facing.
 
-        [SerializeField] private float maxSpeed = 10f; // The fastest the player can travel in the x axis.
-        [SerializeField] private float jumpForce = 250f; // Amount of force added when the player jumps.	
+        [SerializeField]
+        private float maxSpeed = 10f; // The fastest the player can travel in the x axis.
+        [SerializeField]
+        private float jumpForce = 250f; // Amount of force added when the player jumps.	
 
-        [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
-                                                     // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [Range(0, 1)]
+        [SerializeField]
+        private float crouchSpeed = .36f;
+        // Amount of maxSpeed applied to crouching movement. 1 = 100%
 
-        [SerializeField] private bool airControl = false; // Whether or not a player can steer while jumping;
-        [SerializeField] private LayerMask whatIsGround; // A mask determining what is ground to the character
+        [SerializeField]
+        private bool airControl = false; // Whether or not a player can steer while jumping;
+        [SerializeField]
+        private LayerMask whatIsGround; // A mask determining what is ground to the character
 
         private Transform groundCheck; // A position marking where to check if the player is grounded.
         private float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
@@ -22,7 +28,7 @@ namespace UnitySampleAssets._2D
         private Transform ceilingCheck; // A position marking where to check for ceilings
         private float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator anim; // Reference to the player's animator component.
-		private boolean doorKey = false;
+        private bool doorKey = false;
 
         private void Awake()
         {
@@ -47,25 +53,25 @@ namespace UnitySampleAssets._2D
         public void Move(float move, bool crouch, bool jump)
         {
 
-		
+
 
             //only control the player if grounded or airControl is turned on
             if (grounded || airControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*crouchSpeed : move);
+                move = (crouch ? move * crouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                rigidbody2D.velocity = new Vector2(move*maxSpeed, rigidbody2D.velocity.y);
+                rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !facingRight)
                     // ... flip the player.
                     Flip();
-                    // Otherwise if the input is moving the player left and the player is facing right...
+                // Otherwise if the input is moving the player left and the player is facing right...
                 else if (move < 0 && facingRight)
                     // ... flip the player.
                     Flip();
@@ -92,8 +98,11 @@ namespace UnitySampleAssets._2D
             transform.localScale = theScale;
         }
 
-		public Boolean Key{
-			get{ return true;}
-			set{ doorKey = value;}
-    	}
+        public bool Key
+        {
+            get { return true; }
+            set { doorKey = value; }
+        }
+
+    }
 }
