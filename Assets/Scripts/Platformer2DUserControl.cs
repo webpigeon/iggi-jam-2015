b5beec0697c080d2a8eae0607b1a00fdadb980e7
Joe Cutting <Joe Cutting>
2015-01-24 20:22:54 +0000

    Off for food

--- Assets/Scripts/Platformer2DUserControl.cs
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -8,27 +8,37 @@ namespace UnitySampleAssets._2D
     {
         private PlatformerCharacter2D character;
         private bool jump;
+		public bool paused;
 
         private void Awake()
         {
             character = GetComponent<PlatformerCharacter2D>();
+			paused = false;
         }
 
         private void Update()
         {
-            if(!jump)
-            // Read the jump input in Update so button presses aren't missed.
-            jump = Input.GetButtonDown("Jump");
+            if (!paused) {
+					if (!jump)
+            		// Read the jump input in Update so button presses aren't missed.
+					jump = Input.GetButtonDown ("Jump");
+
+					if (Input.GetKeyDown (KeyCode.Escape)) {
+										Debug.Log ("Escape is pressed");
+					}
+				}
         }
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = Input.GetAxis("Horizontal");
-            // Pass all parameters to the character control script.
-            character.Move(h, crouch, jump);
-            jump = false;
-        }
+						if (!paused) {
+								// Read the inputs.
+								bool crouch = Input.GetKey (KeyCode.LeftControl);
+								float h = Input.GetAxis ("Horizontal");
+								// Pass all parameters to the character control script.
+								character.Move (h, crouch, jump);
+								jump = false;
+						}
+				}
     }
 }
