18248c20156c26770749b77a69108883854b4d14
Matthew Bedder <bedder@gmail.com>
2015-01-24 22:59:04 +0000

    Added comment to TriggerZoneRecord

--- Assets/Scripts/TriggerZoneRecord.cs
+++ Assets/Scripts/TriggerZoneRecord.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TriggerZoneRecord : MonoBehaviour {
     public float lastTrigger = 0;
-    public int id = -1;
+    public int id = -1; // Used for debugging
 
     void OnTriggerEnter2D() {
         lastTrigger = Time.time;
