d9257ddddd3f94a59153c9a2aa716b7b2529267d
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-01-24 10:56:52 +0000

    Moving platforms
    Added logic for moving platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
+	public int minUnits;
+	public int maxUnits;
+	public float currentUnits;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.speed = -0.1f;
+		this.minUnits = -2;
+		this.maxUnits = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (currentUnits < minUnits) {
+			speed = 0.1f;
+		}
+
+		if (currentUnits > maxUnits) {
+			speed = -0.1f;
+		}
+
+		currentUnits += speed;
+		transform.Translate (currentUnits * Time.deltaTime, 0, 0);
 	}
 }
