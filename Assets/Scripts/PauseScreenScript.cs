b02d6a617868bc37a6b4f8bffc240d843e3a0b3f
Joe Cutting <Joe Cutting>
2015-01-25 11:01:17 +0000

    pause screen on level 1

---
+++ Assets/Scripts/PauseScreenScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets._2D;
+
+public class PauseScreenScript : MonoBehaviour {
+
+
+	public static PauseScreenScript Instance;
+	
+	void Awake()
+	{
+		if (Instance) {
+			DestroyImmediate(gameObject);
+		} else {
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+	}
+
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
