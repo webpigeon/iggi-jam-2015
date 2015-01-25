80774c1608bb99151212e8353930a8e2b97a77d6
Joe Cutting <Joe Cutting>
2015-01-25 09:42:25 +0000

    committing because it tells me to

---
+++ Assets/Scripts/Goal1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal1 : MonoBehaviour {
+    public GameObject[] triggerZones;
+    private Level1Logic level1Logic;
+
+    void Awake() {
+        level1Logic = FindObjectOfType<Level1Logic>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        int mostRecentTriggerIndex = 0;
+        float mostRecentTriggerTime = 0;
+        for (int i = 0 ; i < 5 ; i++) {
+            float thisTriggerTime = triggerZones[i].GetComponent<TriggerZoneRecord>().lastTrigger;
+            if (thisTriggerTime > mostRecentTriggerTime) {
+                mostRecentTriggerTime = thisTriggerTime;
+                mostRecentTriggerIndex = i;
+            }
+        }
+        level1Logic.solutions[mostRecentTriggerIndex] = true;
+        level1Logic.resetLevel();
+    }
+}
