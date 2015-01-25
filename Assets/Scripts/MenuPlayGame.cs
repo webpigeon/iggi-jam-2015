67b40ba2edebb487569ad15e8e19e5ca009e158a
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-01-25 10:27:36 +0000

    Game play menu

--- Assets/Scripts/MenuPlayGame.cs
+++ Assets/Scripts/MenuPlayGame.cs
@@ -5,4 +5,9 @@ public class MenuPlayGame : MonoBehaviour {
 	public void OnClickPlay(){
 		Application.LoadLevel ("Level1");
 	}
+
+	public void onClickQuit() {
+		Debug.Log ("maple");
+		Application.Quit ();
+	}
 }
